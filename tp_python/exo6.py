ce4f0a29d8da759655c6788b62aeab434a1dfe6b
Paco :D <mougeot.axel@orange.fr>
2024-09-17 17:01:49 +0200

    tp python cours2

--- tp_python/exo6.py
+++ tp_python/exo6.py
@@ -1,4 +1,4 @@
-
+# %%
 
 # ======== EXO 6 - PLANIFICATION DES TACHES =========
 Plan = list[int] #Contient les i tq si i est dans la liste, la tache d'indice i est sélectionnée
@@ -7,5 +7,5 @@ def max_val(p : Plan):
     """Donne la valeur maximum d'un plan possible.
     
     précondition : p doit etre unplan possible."""
-
+    
     
